f9103dc7baf42c379184c28ce64bc88db6f65187
kwdoijowiqjd <44804669+kwdoijowiqjd@users.noreply.github.com>
2018-11-09 17:44:53 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+const bot1 = new Discord.Client();
+
+ var prefix = "+";
+
+
+
+bot1.on('ready', () => {
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${bot1.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${bot1.guilds.size} " ]`);
+  console.log(`Users! [ " ${bot1.users.size} " ]`);
+  console.log(`channels! [ " ${bot1.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+ 
+ bot1.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  if(!args) return message.channel.send(`${prefix}say <words>`);
+
+if (command == "say") {
+
+message.channel.send(args.join("  "))
+    message.delete();
+  }
+
+
+
+});
+
+
+
+
+bot1.login("MzYxMjE3NjU4MTkwMDM2OTk5.Dr8UDg.fjkMFYs23Lk_ETeapVqnW1gpMYs");
+
+
+
